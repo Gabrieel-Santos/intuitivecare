17e628438b9cbfa565cce5f386075827b765cf19
Gabrieel-Santos <maginovam@gmail.com>
2025-03-26 23:42:24 -0300

    Terminando o teste 4

---
+++ teste-04/backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+
+app = FastAPI()
+
+# Libera o CORS para acesso pelo frontend (Vue)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Carrega o CSV uma única vez ao iniciar a aplicação
+df = pd.read_csv("Relatorio_cadop.csv", sep=";", encoding="utf-8", dtype=str).fillna("")
+
+@app.get("/buscar")
+def buscar_operadora(q: str = Query(..., min_length=2)):
+    resultado = df[df["Razao_Social"].str.contains(q, case=False) | df["Nome_Fantasia"].str.contains(q, case=False)]
+    return resultado.head(10).to_dict(orient="records")
